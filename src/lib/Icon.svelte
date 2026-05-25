bcb79cef6ca5d734863ae61da750ec16a54fb439
bl4ckswordsman <91825788+bl4ckswordsman@users.noreply.github.com>
2026-05-25 14:37:16 +0200

    chore: update asset attributions and reformat Icon component SVG paths

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -57,12 +57,31 @@
         stroke-linecap="round"
         stroke-linejoin="round"
     >
-        <g fill="none" stroke="currentColor" stroke-linecap="round" stroke-width="2.0">
-            <path stroke-linejoin="round" d="m4 12l-.858-1.928a1.8 1.8 0 0 1-.08-1.17C4.13 5.115 7.198 2 12 2s7.87 3.115 8.938 6.902c.109.387.07.797-.08 1.17L20 12" />
-            <path stroke-linejoin="round" d="M18 10a9.96 9.96 0 0 0-6-2a9.96 9.96 0 0 0-6 2" />
-            <path stroke-linejoin="round" d="M15 8c0-4-6-4-6 0m0 9c.5.433 2 .866 3 0c1 .866 2.5.433 3 0" />
-            <path stroke-linejoin="round" d="m18.493 14.5l.5-1.5c.253-.577.819-1.001 1.498-1c1.117.002 1.577 1.04 1.5 2c-.173 2.175-1.115 4-3.498 4c-1.256 2.533-3.596 3.9-6.493 4c-2.897-.1-5.237-1.467-6.493-4c-2.383 0-3.325-1.825-3.499-4c-.076-.96.384-1.998 1.5-2c.68-.001 1.246.423 1.5 1l.499 1.5" />
-            <path d="M9.125 13H9m.25 0a.25.25 0 1 1-.5 0a.25.25 0 0 1 .5 0Zm5.875 0H15m.25 0a.25.25 0 1 1-.5 0a.25.25 0 0 1 .5 0Z" />
+        <g
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-width="2.0"
+        >
+            <path
+                stroke-linejoin="round"
+                d="m4 12l-.858-1.928a1.8 1.8 0 0 1-.08-1.17C4.13 5.115 7.198 2 12 2s7.87 3.115 8.938 6.902c.109.387.07.797-.08 1.17L20 12"
+            />
+            <path
+                stroke-linejoin="round"
+                d="M18 10a9.96 9.96 0 0 0-6-2a9.96 9.96 0 0 0-6 2"
+            />
+            <path
+                stroke-linejoin="round"
+                d="M15 8c0-4-6-4-6 0m0 9c.5.433 2 .866 3 0c1 .866 2.5.433 3 0"
+            />
+            <path
+                stroke-linejoin="round"
+                d="m18.493 14.5l.5-1.5c.253-.577.819-1.001 1.498-1c1.117.002 1.577 1.04 1.5 2c-.173 2.175-1.115 4-3.498 4c-1.256 2.533-3.596 3.9-6.493 4c-2.897-.1-5.237-1.467-6.493-4c-2.383 0-3.325-1.825-3.499-4c-.076-.96.384-1.998 1.5-2c.68-.001 1.246.423 1.5 1l.499 1.5"
+            />
+            <path
+                d="M9.125 13H9m.25 0a.25.25 0 1 1-.5 0a.25.25 0 0 1 .5 0Zm5.875 0H15m.25 0a.25.25 0 1 1-.5 0a.25.25 0 0 1 .5 0Z"
+            />
         </g>
     </svg>
 {:else if name === "Mario & Luigi"}
@@ -85,20 +104,42 @@
 {:else if name === "Kirby"}
     <svg
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
         width={size}
         height={size}
-        class="vinda-icon"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        viewBox="0 0 24 24"
     >
+        <path d="M0 0h24v24H0z" fill="none" />
         <g fill="none">
-            <path fill="#fff" d="M15.747 4.727L12 2.71L8.253 4.726L.73 2.22v3.92l3.92 3.92v5.39L12 19.37l7.35-3.92v-5.39l3.92-3.92V2.22z" />
-            <path fill="#bbd8ff" d="M6.784 16.565L4.16 18.86l.49 2.94l3.43-.49l.82-3.613zm10.43 0l2.626 2.295l-.49 2.94l-3.43-.49l-.82-3.613zM9.55 14.45V12h4.9v2.45L12 15.92z" />
-            <path fill="#bbd8ff" d="m4.65 15.43l1.47-.98L12 17.39l5.88-2.94l1.47.98L12 19.35z" />
-            <path stroke="#092f63" stroke-linecap="round" stroke-linejoin="round" d="M5.63 6.12L12 2.69l6.37 3.43" />
-            <path stroke="#092f63" stroke-linecap="round" stroke-linejoin="round" d="M8.254 4.708L.73 2.2v3.92l3.92 3.92v5.39L12 19.35l7.35-3.92v-5.39l3.92-3.92V2.2l-7.524 2.508M9.55 8.08v1.96m4.9-1.96v1.96" />
-            <path stroke="#092f63" stroke-linecap="round" stroke-linejoin="round" d="M9.55 14.45V12h4.9v2.45L12 15.92zM6.61 12h1.017m8.746 0h1.017M6.784 16.565L4.16 18.86l.49 2.94l3.43-.49l.82-3.613zm10.43 0l2.626 2.295l-.49 2.94l-3.43-.49l-.82-3.613z" />
+            <path
+                fill="#fff"
+                d="M15.747 4.727L12 2.71L8.253 4.726L.73 2.22v3.92l3.92 3.92v5.39L12 19.37l7.35-3.92v-5.39l3.92-3.92V2.22z"
+            />
+            <path
+                fill="#bbd8ff"
+                d="M6.784 16.565L4.16 18.86l.49 2.94l3.43-.49l.82-3.613zm10.43 0l2.626 2.295l-.49 2.94l-3.43-.49l-.82-3.613zM9.55 14.45V12h4.9v2.45L12 15.92z"
+            />
+            <path
+                fill="#bbd8ff"
+                d="m4.65 15.43l1.47-.98L12 17.39l5.88-2.94l1.47.98L12 19.35z"
+            />
+            <path
+                stroke="#092f63"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M5.63 6.12L12 2.69l6.37 3.43"
+            />
+            <path
+                stroke="#092f63"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M8.254 4.708L.73 2.2v3.92l3.92 3.92v5.39L12 19.35l7.35-3.92v-5.39l3.92-3.92V2.2l-7.524 2.508M9.55 8.08v1.96m4.9-1.96v1.96"
+            />
+            <path
+                stroke="#092f63"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.55 14.45V12h4.9v2.45L12 15.92zM6.61 12h1.017m8.746 0h1.017M6.784 16.565L4.16 18.86l.49 2.94l3.43-.49l.82-3.613zm10.43 0l2.626 2.295l-.49 2.94l-3.43-.49l-.82-3.613z"
+            />
         </g>
     </svg>
 {:else}
